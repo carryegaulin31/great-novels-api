3f746f3ebfd2554baa0cd781623e80af69ee98e5
carrye <carryegaulin033178@gmail.com>
2020-12-04 12:45:19 -0500

    models declare in models index

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const Sequelize = require('sequelize')
+const AuthorsModel = require('./authors')
+const GenresModel = require('./genres')
+const NovelsModel = require('./novels') 
+const NovelsGenresModel = require('./novelsGenres')
 const connection = new Sequelize('greatBook', 'greatBook', 'bOoK', {
   host: 'localhost', dialect: 'mysql'
 })
+
+const authors = AuthorsModel(connection, Sequelize)
+const genres = GenresModel(connection, Sequelize)
+const novels = NovelsModel(connection, Sequelize)
+const novelsGenres = NovelsGenresModel(connection, Sequelize, genres, novels)
 
