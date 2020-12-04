169ee340631691d0cf55dda5c435fe0c7716357f
carrye <carryegaulin033178@gmail.com>
2020-12-04 13:45:41 -0500

    models index updated

--- models/index.js
+++ models/index.js
@@ -3,6 +3,10 @@ const AuthorsModel = require('./authors')
 const GenresModel = require('./genres')
 const NovelsModel = require('./novels') 
 const NovelsGenresModel = require('./novelsGenres')
+const authors = require('./authors')
+const genres = require('./genres')
+const novels = require('./novels')
+const novelsGenres = require('./novelsGenres')
 const connection = new Sequelize('greatBook', 'greatBook', 'bOoK', {
   host: 'localhost', dialect: 'mysql'
 })
@@ -12,3 +16,13 @@ const Genres = GenresModel(connection, Sequelize)
 const Novels = NovelsModel(connection, Sequelize)
 const NovelsGenres = NovelsGenresModel(connection, Sequelize, genres, novels)
 
+module.exports = {
+  authors,
+  genres,
+  novels,
+  novelsGenres,
+  AuthorsModel,
+  GenresModel,
+  NovelsModel,
+  NovelsGenresModel
+}
