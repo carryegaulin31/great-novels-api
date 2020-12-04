0b255f14b94f3832a6497752a6f586950a8bc01a
carrye <carryegaulin033178@gmail.com>
2020-12-04 14:24:17 -0500

    getAllNovels add

--- controllers/novels.js
+++ controllers/novels.js
@@ -1,1 +1,15 @@
 const models = require('../models')
+
+const getAllNovels = async (request, response) => {
+  const { id } = request.params
+
+  const novels = await models.Novels.findAll({
+    where: { id }
+  })
+
+  return novels
+    ? response.send(novels)
+    : response.sendStatus('Sorry cannot find Novels')
+}
+
+module.exports = getAllNovels
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const { getAllAuthors, getAllAuthorsWithNovelAndGenre } = require('./controllers/authors')
 const getAllGenres = require('./controllers/genres')
+const getAllNovels = require('./controllers/novels')
 
 const app = express()
+
 app.get('/:id', getAllAuthors)
 app.get('/:id', getAllGenres)
+app.get('/:id', getAllNovels)
 app.get('/', getAllAuthorsWithNovelAndGenre)
 
 app.listen(1337, () => {
